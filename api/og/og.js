bfab999926fd4f955d333140f5166b761e15c832
jh3y <jh3y@users.noreply.github.com>
2022-10-30 18:17:05 +0000

    feat: try switching it over to require

--- api/og/og.js
+++ api/og/og.js
@@ -1,25 +1,25 @@
-import satori from 'satori';
-import {html as toReactElement} from 'satori-html';
-import fs from 'fs';
-import { Buffer } from 'node:buffer';
-import sharp from 'sharp';
+const satori = require('satori').default;
+const { html } = require('satori-html');
+const sharp = require('sharp');
+const fs = require('fs');
+const { Buffer } = require('node:buffer');
 
 const height = 630;
 const width = 1200;
 
 const fontFile = fs.readFileSync(`${process.cwd()}/public/fonts/Roboto/Roboto-Black.ttf`);
 
-export const handler = async () => {
-  const html = toReactElement(`
+exports.handler = async function() {
+  const markup = html(`
     <div style="height: 100%;width: 100%;display: flex;align-items: center;justify-content: center;font-size: 72px;color: white;background: black;">
       Jhey made some OG
       <img src="https://picsum.photos/100/100?random=10">
     </div>
   `)
-  const svg = await satori(html, {
+  const svg = await satori(markup, {
     fonts: [
       {
-        name: 'Inter Latin',
+        name: 'Roboto',
         data: fontFile,
         style: 'normal'
       }
