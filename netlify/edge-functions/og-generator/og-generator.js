f5c137ace782007809befb5a48af17564dbc64cd
jh3y <jh3y@users.noreply.github.com>
2022-10-31 00:05:05 +0000

    feat: strip things a little

--- netlify/edge-functions/og-generator/og-generator.js
+++ netlify/edge-functions/og-generator/og-generator.js
@@ -10,7 +10,6 @@ export default async (request) => {
   // const fontFile = fs.readFileSync(`${process.cwd()}/public/fonts/Roboto/Roboto-Black.ttf`);
   const markup = html('<div style="height: 100%;width: 100%;display: flex;align-items: center;justify-content: center;font-size: 72px;color: white;background: black;">Jhey makes OG</div>')
   const svg = await satori(markup, {
-    embedFont: false,
     height,
     width
   });
@@ -22,13 +21,13 @@ export default async (request) => {
     }
   }).render().asPng()
 
-  return new Response (png, {
+  return Response(png, {
     statusCode: 200,
     headers: {
-      'Cache-Control': 'public, immutable, no-transform, max-age=31536000',
+      // 'Cache-Control': 'public, immutable, no-transform, max-age=31536000',
       'Content-Type': 'image/png',
     },
-    isBase64Encoded: true,
+    // isBase64Encoded: true,
     // body: png,
   })
 }
