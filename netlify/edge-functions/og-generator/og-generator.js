da7e0e59e79b587e02ee8ababa85145f9c01b106
jh3y <jh3y@users.noreply.github.com>
2022-11-01 23:45:56 +0000

    feat: deploy working Edge function

--- netlify/edge-functions/og-generator/og-generator.js
+++ netlify/edge-functions/og-generator/og-generator.js
@@ -1,25 +1,25 @@
-import satori from 'https://cdn.skypack.dev/satori';
-import { html } from 'https://cdn.skypack.dev/satori-html';
-import sharp from 'https://cdn.skypack.dev/sharp';
+// import satori from 'https://cdn.skypack.dev/satori';
+// import { html } from 'https://cdn.skypack.dev/satori-html';
+// import sharp from 'https://cdn.skypack.dev/sharp';
 
-const height = 630;
-const width = 1200;
+// const height = 630;
+// const width = 1200;
 
 
-export default async (request) => {
-  // const fontFile = fs.readFileSync(`${process.cwd()}/public/fonts/Roboto/Roboto-Black.ttf`);
-  const markup = html('<div style="height: 100%;width: 100%;display: flex;align-items: center;justify-content: center;font-size: 72px;color: white;background: black;">Jhey makes OG</div>')
-  const svg = await satori(markup, {
-    height,
-    width
-  });
+// export default async (request) => {
+//   // const fontFile = fs.readFileSync(`${process.cwd()}/public/fonts/Roboto/Roboto-Black.ttf`);
+//   const markup = html('<div style="height: 100%;width: 100%;display: flex;align-items: center;justify-content: center;font-size: 72px;color: white;background: black;">Jhey makes OG</div>')
+//   const svg = await satori(markup, {
+//     height,
+//     width
+//   });
 
-  const res = await (await sharp(Buffer.from(svg)).png().toBuffer()).toString('base64')
+//   const res = await (await sharp(Buffer.from(svg)).png().toBuffer()).toString('base64')
 
-  return new Response (res, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'image/png',
-    },
-  })
-}
+//   return new Response (res, {
+//     statusCode: 200,
+//     headers: {
+//       'Content-Type': 'image/png',
+//     },
+//   })
+// }
---
+++ netlify/edge-functions/og/og.jsx
@@ -0,0 +1,22 @@
+import React from "https://esm.sh/react@18.2.0";
+import { ImageResponse } from 'https://deno.land/x/og_edge/mod.ts'
+
+export default async function handler(req) {
+    return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: 128,
+          background: 'lavender',
+        }}
+      >
+        {Date.now()}
+      </div>
+    )
+  )
+}
